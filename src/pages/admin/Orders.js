30b5ec562232275a696f39610a121d270548278d
mohid <mohid10587@gmail.com>
2022-11-29 10:58:18 +0600

    ok

--- src/pages/admin/Orders.js
+++ src/pages/admin/Orders.js
@@ -39,7 +39,8 @@ const Orders = () => {
 
 
       <div className='px-4'>
-        {orderedVouchers?.map(px => <div key={px._id} className='mb-10 sm:flex justify-between border-2 border-pink-700 p-4 relative'>
+        {orderedVouchers?.map(px => <div key={px._id} className='mb-10  border-2 border-pink-700  relative'>
+          <div className='sm:flex justify-between'>
           <div className='sm:w-1/2'>
             <p className=''><span className='font-bold mr-2'>Customer's Name:</span>{px.name}</p>
             <p className=''><span className='font-bold mr-2'>Send Money from:</span>{px.bkashNumber}</p>
@@ -49,24 +50,30 @@ const Orders = () => {
             <p className=''><span className='font-bold mr-2'>Address:</span>{px.address} , {px.city}</p>
 
           </div>
-          <div className='sm:w-1/2 relative mb-5'>
-            <p className='font-bold'>Order Summary</p>
-            
+          <div className='sm:w-1/2 relative '>
+            <div className='  mb-5 border-b-2 border-black '>
+              <p className='font-bold'>Order Summary</p>
+
               {px.orderedProduct?.map(p => <div className='flex justify-between' key={p._id}>
                 <img src={p.img} className='w-4' alt="" />
                 <p>{p.name}</p>
                 <p>{p.quantity}pcs x ${p.price}</p>
-           
+
                 <p>=${p.quantity * p.price}</p>
 
 
               </div>)}
-           
+              <p></p>
 
-            <button className='hidden sm:block absolute bottom-0 right-0 bg-red-700 px-3 py-1 rounded-md text-white font-bold' onClick={() => deleteVoucher(px._id)}>Delete</button>
+
+            </div>
+
           </div>
-          <button className='sm:hidden block absolute bottom-0  bg-red-700 px-3 py-1 rounded-md text-white font-bold' onClick={() => deleteVoucher(px._id)}>Delete</button>
+          </div>
+          <div className='   w-full flex justify-end'>
+            <button className='   bg-red-700 px-3 py-1 rounded-md text-white font-bold' onClick={() => deleteVoucher(px._id)}>Delete</button>
 
+          </div>
         </div>)}
 
       </div>
